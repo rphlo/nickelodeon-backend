cf51e2bb484678b4c0323f7838a74cf8d2a807f3
Raphaël Stefanini <raphael.stefanini@qvantel.com>
2014-08-07 05:10:57 +0300

    utf-8 in filename

--- nickelodeon/views.py
+++ nickelodeon/views.py
@@ -41,7 +41,7 @@ def download_song(request, pk, extension=None):
         return HttpResponse(status=404)
     mime = 'audio/mpeg' if extension == 'mp3' else 'audio/aac'
     file_path = re.sub(r'(mp3$)', extension, file_path)
-    file_path = "/internal%s" % file_path
+    file_path = "/internal%s" % file_path.encode('utf-8')
     return x_accel_redirect(request, file_path, mime=mime)
 
 
