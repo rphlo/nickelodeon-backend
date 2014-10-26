a5680695cbde4f3219bb83e1d3de8867b9a100e1
Raphaël Stefanini <raphael.stefanini@qvantel.com>
2014-10-26 19:21:57 +0200

    modified copy method

--- nickelodeon/tasks.py
+++ nickelodeon/tasks.py
@@ -3,12 +3,73 @@ import datetime
 import pafy
 import re
 import os
+import subprocess
+from shutil import copystat
 from django.utils.translation import ugettext as _
 from django.conf import settings
+from django.core.files import locks
 from celery import shared_task, current_task
-import subprocess
-from django.core.files.move import file_move_safe
 from nickelodeon.models import Song
+
+
+def file_move_safe(old_file_name, new_file_name, chunk_size=1024 * 64,
+                   allow_overwrite=False):
+    """
+    Moves a file from one location to another in the safest way possible.
+
+    First, tries ``os.rename``, which is simple but will break across
+    filesystems.
+    If that fails, streams manually from one file to another in pure Python.
+
+    If the destination file exists and ``allow_overwrite`` is ``False``, this
+    function will throw an ``IOError``.
+    """
+    # There's no reason to move if we don't have to.
+    if os.path.samefile(old_file_name, new_file_name):
+        return
+
+    try:
+        # If the destination file exists and allow_overwrite is False then
+        # raise an IOError
+        if not allow_overwrite and os.access(new_file_name, os.F_OK):
+            raise IOError("Destination file %s exists and allow_overwrite is "
+                          "False" % new_file_name)
+
+        os.rename(old_file_name, new_file_name)
+        return
+    except OSError:
+        # This will happen with os.rename if moving to another filesystem
+        # or when moving opened files on certain operating systems
+        pass
+
+    # first open the old file, so that it won't go away
+    with open(old_file_name, 'rb') as old_file:
+        # now open the new file, not forgetting allow_overwrite
+        fd = os.open(new_file_name,
+                     (os.O_WRONLY | os.O_CREAT | getattr(os, 'O_BINARY', 0) |
+                     (os.O_EXCL if not allow_overwrite else 0)))
+        try:
+            locks.lock(fd, locks.LOCK_EX)
+            current_chunk = None
+            while current_chunk != b'':
+                current_chunk = old_file.read(chunk_size)
+                os.write(fd, current_chunk)
+        finally:
+            locks.unlock(fd)
+            os.close(fd)
+    try:
+        copystat(old_file_name, new_file_name)
+    except OSError:
+        pass
+    try:
+        os.remove(old_file_name)
+    except OSError as e:
+        # Certain operating systems (Cygwin and Windows)
+        # fail when deleting opened files, ignore it.  (For the
+        # systems where this happens, temporary files will be auto-deleted
+        # on close anyway.)
+        if getattr(e, 'winerror', 0) != 32 and getattr(e, 'errno', 0) != 13:
+            raise
 
 
 def convert_audio(input_file, output_file_aac, output_file_mp3, callback=None):
@@ -42,7 +103,7 @@ def convert_audio(input_file, output_file_aac, output_file_mp3, callback=None):
     read_progress_str_index = 0
     while True:
         out = process.stdout.read(1)
-        if out == '' and process.poll() != None:
+        if out == '' and process.poll() is not None:
             break
         if out != '':
             if not duration_read:
@@ -118,7 +179,8 @@ def fetch_youtube_video(video_id=''):
     )
     aac_path = os.path.join(dst_folder, safe_title+".aac")
     mp3_path = os.path.join(dst_folder, safe_title+".mp3")
-    audio_stream.download(download_path, callback=update_dl_progress, quiet=True)
+    audio_stream.download(download_path, callback=update_dl_progress,
+                          quiet=True)
     update_conversion_progress(0)
     convert_audio(download_path, aac_tmp_path, mp3_tmp_path,
                   callback=update_conversion_progress)
@@ -127,6 +189,7 @@ def fetch_youtube_video(video_id=''):
         os.makedirs(dst_folder)
     file_move_safe(aac_tmp_path, aac_path)
     file_move_safe(mp3_tmp_path, mp3_path)
+    os.remove(download_path)
     song = Song(filename=aac_path[len(settings.MEDIA_ROOT):-4],
                 title=safe_title)
     song.save()
