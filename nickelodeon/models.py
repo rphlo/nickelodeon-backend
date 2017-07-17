05dece9dd6284c56441d24641f842ef26502d243
Raphaël Stefanini <rphl@rphl.net>
2017-07-17 17:32:13 +0000

    fix error in generating ids

--- nickelodeon/models.py
+++ nickelodeon/models.py
@@ -17,7 +17,7 @@ AVAILABLE_FORMATS = ('mp3', 'aac')
 
 def random_key():
     rand_bytes = bytes(struct.pack('Q', random.getrandbits(64)))
-    b64 = base64.b64encode(rand_bytes)
+    b64 = base64.b64encode(rand_bytes).decode('utf-8')
     b64 = b64[:11]
     b64 = b64.replace('+', '-')
     b64 = b64.replace('/', '_')
