d45471e52373a1056d1f3adbf31445e2e0b2ed45
Raphaël Stefanini <rphl@rphl.net>
2018-02-20 18:10:11 +0200

    Make Service worker so that PWA is available offline

--- nickelodeon/static/sw.js
+++ nickelodeon/static/sw.js
@@ -1,14 +1,27 @@
 self.addEventListener('install', function(e){
+ var timeStamp = Date.now();
  e.waitUntil(
    caches.open('humppakone').then(function(cache){
      return cache.addAll([
        '/',
-       '/index.html',
-       '/index.js',
-       '/index.css',
-       '/static/background.mp4'
-       '/static/background.webm'
-     ]);
+       '/index.html?ts='+timeStamp,
+       '/index.js?ts='+timeStamp,
+       '/index.css?ts='+timeStamp,
+       '/background.mp4?ts='+timeStamp,
+       '/background.webm?ts='+timeStamp
+     ]).then(function(){self.skipWaiting()});
    })
  );
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch: true}).then(function(response){
+      return response || fetch(event.request);
+    })
+  );
 });
