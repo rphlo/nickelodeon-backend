c448abc9653aa4e564124a7b7bb6c048adf832af
Raphaël Stefanini <raphael.stefanini@nidos.fi>
2018-07-16 16:18:47 +0300

    UI improvements

--- nickelodeon/static/sw.js
+++ nickelodeon/static/sw.js
@@ -1,4 +1,4 @@
-var VERSION_NAME = 'humppakone_v2018071600';
+var VERSION_NAME = 'humppakone_v2018071601';
 self.addEventListener('install', function(e){
  var timeStamp = Date.now();
  e.waitUntil(
