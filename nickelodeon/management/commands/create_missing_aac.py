09a8d9cb4378be922fd609575f6b48225f95e556
Raphaël Stefanini <rphl@rphl.net>
2019-08-16 19:40:35 +0300

    rewrite create_missing_aac command

--- nickelodeon/management/commands/create_missing_aac.py
+++ nickelodeon/management/commands/create_missing_aac.py
@@ -1,9 +1,10 @@
 import os
+import tempfile
 
 from django.core.management.base import BaseCommand
 
 from nickelodeon.models import MP3Song
-from nickelodeon.utils import convert_audio
+from nickelodeon.utils import convert_audio, s3_object_url, s3_upload
 
 
 class Command(BaseCommand):
@@ -23,13 +24,14 @@ class Command(BaseCommand):
         if not song.has_aac and song.has_mp3:
             mp3_path = song.get_file_format_path('mp3')
             aac_path = song.get_file_format_path('aac')
-            self.stdout.write('Converting {}'.format(mp3_path))
+            mp3_url = s3_object_url(mp3_path)
+            with tempfile.NamedTemporaryFile() as aac_file:
+                aac_tmp_path = aac_file.name
             convert_audio(
-                mp3_path,
-                output_file_aac=aac_path,
-                callback=self.print_conversion_progress
+                mp3_url,
+                output_file_aac=aac_tmp_path,
             )
-            self.stdout.write('\nDone')
+            s3_upload(aac_tmp_path, aac_path)
         if not song.aac:
             song.aac = True
             song.save()
