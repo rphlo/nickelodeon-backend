443afd740e32355f666101be03aedc197453768a
Raphaël Stefanini <raphael@rphlo.com>
2021-11-24 16:29:21 +0200

    Fix issue with utf8

--- nickelodeon/resumable_file.py
+++ nickelodeon/resumable_file.py
@@ -1,6 +1,6 @@
 
 from resumable.files import ResumableFile as OrigResumableFile
-
+from django.utils import slugify
 
 class ResumableFile(OrigResumableFile):
     def chunk_names(self):
@@ -10,3 +10,14 @@ class ResumableFile(OrigResumableFile):
         for name in file_names:
             if name[:-4] == pattern:
                 yield name
+
+    @property
+    def filename(self):
+        """Gets the filename."""
+        filename = self.kwargs.get('resumableFilename')
+        if '/' in filename:
+            raise Exception('Invalid filename')
+        return "%s_%s" % (
+            self.kwargs.get('resumableTotalSize'),
+            slugify(filename)
+        )
