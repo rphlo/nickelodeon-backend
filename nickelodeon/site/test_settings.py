b7304f632106221c5709cb7286a4d82aa57bc980
Raphael Stefanini <raphael@rphlo.com>
2022-07-21 08:32:04 +0300

    Update ci

--- nickelodeon/site/test_settings.py
+++ nickelodeon/site/test_settings.py
@@ -21,8 +21,6 @@ DATABASES = {
     }
 }
 
-S3_ENDPOINT_URL = "https://s3.wasabisys.com"
-if os.environ.get("S3_ACCESS_KEY"):
-    S3_ACCESS_KEY = os.environ.get("S3_ACCESS_KEY")
-if os.environ.get("S3_SECRET_KEY"):
-    S3_SECRET_KEY = os.environ.get("S3_SECRET_KEY")
+S3_ENDPOINT_URL = "http://localhost:9000"
+S3_ACCESS_KEY = "minioadmin"
+S3_SECRET_KEY = "minioadmin"
